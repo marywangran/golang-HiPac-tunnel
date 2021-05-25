dbdfb2d69b6d8c3387b7f4465e68de0a17d207ad
zhaoya <marywangran@126.com>
2021-05-25 10:44:31 +0800

    Update main.go

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"log"
+	"net"
 	"net/http"
 	_ "net/http/pprof"
 	"strconv"
@@ -21,22 +22,20 @@ func main() {
 	var client bool = false
 	var queues int = 4
 	var addr [4]byte
-	var ip1, ip2, ip3, ip4 int
 	var key	int
 
-	if len(os.Args) == 8 {
+	if len(os.Args) == 5 {
 		if os.Args[1] == "client" {
 			client = true
 		}
 		queues, _ = strconv.Atoi(os.Args[2])
-		ip1, _ = strconv.Atoi(os.Args[3])
-		ip2, _ = strconv.Atoi(os.Args[4])
-		ip3, _ = strconv.Atoi(os.Args[5])
-		ip4, _ = strconv.Atoi(os.Args[6])
-		addr = [4]byte{byte(ip1), byte(ip2), byte(ip3), byte(ip4)}
-		key, _ = strconv.Atoi(os.Args[7])
+		ip := net.ParseIP(os.Args[3]).To4()
+		for i := 0; i < len(ip); i += 1 {
+			addr[i] = ip[i]
+		}
+		key, _ = strconv.Atoi(os.Args[4])
 	} else {
-		fmt.Println("./tuntap server|client(mode) 10(queues) 192 168 56 1(IP address) key(pre shared key)")
+		fmt.Println("./tuntap server|client(mode) 10(queues) 192.168.56.1(IP address) key(pre shared key)")
 		os.Exit(1)
 	}
 	go func() {
